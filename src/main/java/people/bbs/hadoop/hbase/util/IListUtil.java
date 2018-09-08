a2555375d33905a6189c21304e7980c3ac2ce635
wizardyu <wizardyu@DESKTOP-VM30U3R>
2018-09-08 22:08:19 +0800

    测试

---
+++ src/main/java/people/bbs/hadoop/hbase/util/IListUtil.java
@@ -0,0 +1,16 @@
+package people.bbs.hadoop.hbase.util;
+import java.util.List;
+
+public class IListUtil {
+
+	public static String implode(List<String> strList) {
+		String res = "";
+		if (strList == null || strList.size() == 0) {
+			return res;
+		}
+		for (String str : strList) {
+			res += str + ",";
+		}
+		return res.substring(0, res.length() - 1);
+	}
+}
